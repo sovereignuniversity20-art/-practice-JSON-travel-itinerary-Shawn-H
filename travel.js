045fc1c46f355d88799af9be4a13879b9f492200
Hero <sovereign.university20@gmail.com>
2026-06-17 14:44:36 -0500

    2 travelers with 2 destinations and 2 activities per destination

---
+++ travel.js
@@ -0,0 +1,50 @@
+const summerTrip = {
+    "traveler1": {
+        "name": "Shawn",
+        "contact": {
+            "email": "shawntravels@gmail.com", 
+            "phone": "314-413-3141"},
+        "destination1": {
+            "location": "Tokyo, Japan",
+            "arrival": "2026-07-05",
+            "departure": "2027-01-31",
+            "activities": {
+                "name": "Hiking the mountains",
+                "time": "Swimming in the lake"
+            }
+        },
+        "destination2": {
+            "location": "Accra, Ghana",
+            "arrival": "2027-01-31",
+            "departure": "2027-03-31",
+            "activities": {
+                "name": "Hiking the volcano",
+                "time": "Shopping in the merchant district"
+            }
+    }   
+},
+    "traveler2": {
+        "name": "Denecia",
+        "contact": {
+            "email": "deedeetravels@gmail.com", 
+            "phone": "314-523-3231"},
+        "destination1": {
+            "location": "Tokyo, Japan",
+            "arrival": "2026-07-05",
+            "departure": "2027-01-31",
+            "activities": {
+                "name": "Shopping downtown",
+                "time": "Dinner on the rooftop"
+            }
+        },
+        "destination2": {
+            "location": "Accra, Ghana",
+            "arrival": "2027-01-31",
+            "departure": "2027-03-31",
+            "activities": {
+                "name": "Hiking the volcano",
+                "time": "Shopping in the merchant district"
+            }
+    }   
+}
+}
